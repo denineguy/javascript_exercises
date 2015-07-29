ecba7f30c18baaba46a598dccc3d48f116120c19
Denine Guy <denineguy@gmail.com>
2015-07-28 23:15:16 -0400

    updated ex05

--- javascript_exercises/ex05.js
+++ javascript_exercises/ex05.js
@@ -6,15 +6,18 @@ var insertDashes = function() {
   var userInput = prompt('Please enter a number that is at least 5 digits long');
   var dashedNumber = []; 
   var numToString = userInput.toString(); 
-  for(var i = 0; i<numToString.length; i++) {  
-    if(numToString[i] % 2 ===0 && numToString[i+1] % 2 === 0) { 
+  for(var i = 0; i < numToString.length; i++) {  
+    if(numToString[i] % 2 === 0 && numToString[i+1] % 2 === 0) { 
       dashedNumber.push(numToString[i] + '-'); 
     } else { 
       dashedNumber.push(numToString[i]); 
     } 
   } 
   dashedNumber = dashedNumber.join(''); 
-  console.log(dashedNumber); 
+  return(dashedNumber); 
 }
 
 insertDashes();
+
+
+//Try to refactor or think of an easier way//
