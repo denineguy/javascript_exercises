b607ff38ea9f0f00a0ed08e85191a7437679c781
Denine Guy <denineguy@gmail.com>
2015-08-03 20:37:19 -0400

    updated with exercise 7

--- javascript_exercises/ex07.js
+++ javascript_exercises/ex07.js
@@ -1,3 +1,24 @@
 /* W3Resource.com: Write a JavaScript program to convert temperatures to and from
 celsius, fahrenheit.[ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f =
 temperature in fahrenheit ] Expected Output : 60°C is 140 °F 45°F is 7.222222222222222°C */
+
+
+function temperature(rawTemp, scale) { 
+  var newTemp; 
+  if (scale === 'celsius') { 
+    newTemp =  (rawTemp * 9/5) + 32; 
+    return newTemp + '\xB0C' 
+  } else { 
+    newTemp =  (rawTemp - 32) * 5/9; 
+    return newTemp + '\xB0F' 
+  } 
+ 
+}
+
+temperature(60, 'celsius');
+temperature(45, 'fahrenheit');
+
+
+
+
+
